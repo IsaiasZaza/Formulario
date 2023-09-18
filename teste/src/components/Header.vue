fa591ce08d2c3bc23f8d704ff2c81b044428a85d
IsaiasZaza <131933508+IsaiasZaza@users.noreply.github.com>
2023-09-18 10:40:48 -0300

    up

---
+++ teste/src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <header>
+        header
+    </header>
+</template>
+
+<script>
+    export default {
+       
+        }
+    
+</script>
+    .header {
+
+    }
+<style>
+</style>
